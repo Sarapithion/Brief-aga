5955cb0fb9996e8796610078bdf6569a89ef3c23
sara <sara.pithion.simplon@gmail.com>
2024-04-15 16:57:10 +0200

    second

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,1 +1,32 @@
-//Vérification des champs
+function signin() {
+    let email = document.querySelector('.emailLogin').value
+    let password = document.querySelector('.passwordLogin').value
+
+    let user = {
+        email_user: email,
+        password_user: password,
+    }
+
+    let params = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json; charset=utf-8',
+        },
+        body: JSON.stringify(user),
+    }
+
+    fetch('./treatments/login.php', params)
+        .then((res) => res.text())
+        .then((data) => {
+            if (data === 'connected') {
+                let loginForm = document.querySelector('.loginForm')
+                loginForm.classList.add('hidden')
+            } else {
+                console.log('no accounts')
+            }
+            console.log(data)
+        })
+        .catch((e) => {
+            alert('failed')
+        })
+}
--- src/Views/Connexion.php
+++ src/Views/Connexion.php
@@ -4,7 +4,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdn.tailwindcss.com"></script>
     <title>Application de gestion</title>
     <link rel="stylesheet" href="assets/css/styles.css">
 </head>
@@ -28,42 +27,43 @@
 
     </header>
     <main>
-        <div class="flex items-center justify-center h-screen">
+        <div class=" flex items-center justify-center h-screen">
 
-            <div class=" w-1/4 bg-slate-50 flex flex-col justify-center p-8 ">
-                <div class=" sm:mx-auto sm:w-full sm:max-w-sm">
-                    <h2 class=" text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Bienvenue</h2>
-                </div>
-
-                <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-                    <form class="space-y-6" action="#" method="POST">
-                        <div>
-                            <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email</label>
-                            <div class="mt-2">
-                                <input id="email" name="email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-                            </div>
+                    <div class=" w-1/4 bg-slate-50 flex flex-col justify-center p-8 ">
+                        <div class=" sm:mx-auto sm:w-full sm:max-w-sm">
+                            <h2 class=" text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Bienvenue</h2>
                         </div>
 
-                        <div>
-                            <div class="flex items-center justify-between">
-                                <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Mot de passe</label>
-                            </div>
-                            <div class="mt-2">
-                                <input id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-                            </div>
-                        </div>
+                        <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                            <form class="space-y-6" action="#" method="POST">
+                                <div>
+                                    <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email</label>
+                                    <div class="mt-2">
+                                        <input id="email" name="email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                                    </div>
+                                </div>
 
-                        <div>
-                            <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Connexion</button>
+                                <div>
+                                    <div class="flex items-center justify-between">
+                                        <label for="password" class="block text-sm font-medium leading-6 text-gray-900" value="passwordLogin">Mot de passe</label>
+                                    </div>
+                                    <div class="mt-2">
+                                        <input id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                                    </div>
+                                </div>
+
+                                <div>
+                                    <button type="submit" onclick="signin" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Connexion</button>
+                                </div>
+                            </form>
                         </div>
-                    </form>
-                </div>
+                    </div>
+
             </div>
 
-        </div>
-
-    </main>
+            </main>
 
 </body>
-
+<script src="https://cdn.tailwindcss.com"></script>
+<script src="../../public/assets/js/script.js"></script>
 </html>
